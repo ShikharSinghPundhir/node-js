dcd6c98f5c5cdd7a338dbeb9689c4569a88ee0ed
ShikharSinghPundhir <shikharpundhir84@gmail.com>
2022-11-15 21:49:51 +0530

    n

--- filesystem.js
+++ filesystem.js
@@ -3,3 +3,8 @@ const fs=require('fs')
 fs.readFile(__dirname+"/pn.txt",(data)=>{
     console.log(data)
 })
+fs.readFile(__dirname+"/pn.txt","utf8",(err,data)=>{   //utf8 file format hota //callback es6
+    if(err) throw err;
+    console.log(data);
+
+});
