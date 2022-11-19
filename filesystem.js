4cbe7db05e5ed902adbf268026e7fce8c66ad156
ShikharSinghPundhir <shikharpundhir84@gmail.com>
2022-11-19 21:37:38 +0530

    m

--- filesystem.js
+++ filesystem.js
@@ -1,10 +1,21 @@
 const fs=require('fs')
 //console.log(fs)
-fs.readFile(__dirname+"/pn.txt",(data)=>{
-    console.log(data)
-})
-fs.readFile(__dirname+"/pn.txt","utf8",(err,data)=>{   //utf8 file format hota //callback es6
-    if(err) throw err;
-    console.log(data);
+// fs.readFile(__dirname+"/pn.txt",(data)=>{
+//     console.log(data)
+// })
+// fs.readFile(__dirname+"/pn.txt","utf8",(err,data)=>{   //utf8 file format hota //callback es6
+//     if(err) throw err;
+//     console.log(data);
 
-});
+// });
+// console.log('hello')
+
+
+try{
+    const data =fs.readFileSync(__dirname+"/pn.txt","utf-8");
+    console.log(data);
+}
+catch(err){
+console.log(err)
+}
+console.log('hello')
